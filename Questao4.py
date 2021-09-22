7762210f36a2d2250020e60cacaa12899106cef7
mateusmoreira32 <74328621+mateusmoreira32@users.noreply.github.com>
2021-09-22 20:04:50 -0300

    Create Questao4.py

---
+++ Questao4.py
@@ -0,0 +1,6 @@
+palav = input("Informe uma palavra: ").title()
+
+if palav.endswith('o'):
+    print(f"{palav}s")
+else:
+    print(f"Essa conjunção de  Palavra '{palav}' não é terminada la letra O!")
